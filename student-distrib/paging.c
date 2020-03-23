ebe9b416800ce5a2fc16d1821e88ca5d552dcd5c
unknown <mrnolan3@illinois.edu>
2020-03-23 13:00:54 -0400

    Fix compilation error

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -1,8 +1,8 @@
 #include "paging.h"
 #include "types.h"
 
-uint32_t page_directory[PAGE_SIZE] __attribute__((aligned (PAGE_SIZE)));
-uint32_t page_table[PAGE_SIZE] __attribute__((aligned (PAGE_SIZE)));
+uint32_t page_directory[TABLE_SIZE] __attribute__((aligned (PAGE_SIZE)));
+uint32_t page_table[TABLE_SIZE] __attribute__((aligned (PAGE_SIZE)));
 
 
 /*
@@ -17,14 +17,17 @@ uint32_t page_table[PAGE_SIZE] __attribute__((aligned (PAGE_SIZE)));
 void init_paging(){
     int i;
 
+	// Initialize the page table at 0-4 MB so only the video memory page is present
     for(i = 0; i < TABLE_SIZE; i++){
-        page_table[i] = ((i == VIDEO_LOC/PAGE_SIZE)) ? ((VIDEO_LOC) | (DEFAULT_PTE)) : ((DEFAULT_PTE ^ PTE_P));
+        page_table[i] = (i == VIDEO_LOC/PAGE_SIZE) ? ((VIDEO_LOC) | (DEFAULT_PTE)) : ((DEFAULT_PTE) ^ (PTE_P));
     }
 
-    page_directory[0] = (uint32_t) page_table | PDE_RW | PDE_P;
+	// Add page table to page directory, then add 4MB kernel page to directory
+    page_directory[0] = ((uint32_t) page_table | PDE_RW) | PDE_P;
 
-    page_directory[1] = KERNEL_LOC | PDE_PS | DEFAULT_PTE;
+    page_directory[1] = (KERNEL_LOC | PDE_PS) | DEFAULT_PTE;
 
+	// Fill the rest of the directory with empty 4MB pages 
     for(i = 2; i < TABLE_SIZE; i++){
         page_directory[i] = (i << PDE_4MP_PBA_OFFSET) | PDE_PS | PDE_RW;
     }
