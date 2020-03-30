de823e3a95c67e14ddeb850731b1eb7784d89506
mrnolan3 <mrnolan3@illinois.edu>
2020-03-30 18:00:07 -0500

    Replace tests.c

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -215,24 +215,24 @@ void terminal_keyboard_test() {
  * Coverage: RTC
  * Files: rtc.c
  */
-void rtc_write_test() {
+int rtc_write_test() {
 	TEST_HEADER;
-	uint32_t * testRate;
-	*testRate = DEFAULT_FREQ;
+	uint32_t testRate;
+	testRate = DEFAULT_FREQ;
 	uint32_t i;
 
 	initialize_rtc();
 	rtc_open(0);
 
-	while (*testRate <= FREQ_UPPER_BOUND) {
-		rtc_write(0, testRate);
+	while (testRate <= FREQ_UPPER_BOUND) {
+		rtc_write(0, &testRate);
 
 		// display "test" 25 times at each possibly frequency
 		for (i = 0; i < 25; i++) 
 			rtc_read(0);
 		printf ("SPEEDITUP");
 		
-		*testRate *= POWER_OF_TWO;
+		testRate *= POWER_OF_TWO;
 
 	}
 
@@ -241,18 +241,38 @@ void rtc_write_test() {
       after handling the current interrupt, must read register C */
     outb(REGISTERC, RTCPORT);
     inb(RTCDATA);
+
+	return SUCCESS;
 }
 
-int read_file_test(const int8_t* fname) {
+/* read_dentry_test
+ * 
+ * Inputs: fname -- name of file to read
+ * Outputs: None
+ * Side Effects: Tests read_dentry_by_name and read_dentry_by_index
+ * Coverage: FILE SYSTEM
+ * Files: file_system.c
+ */
+int read_dentry_test(const int8_t* fname) {
 	TEST_HEADER;
     dentry_t file1;
 	dentry_t file2;
+
+	// Find the given file with both name and inode number, then compare to make sure they match
     if(-1 == read_dentry_by_name(fname, &file1)) return FAIL;
 	if(-1 == read_dentry_by_index(file1.inode_num, &file2)) return FAIL;
 	if(file1.file_name != file2.file_name || file1.file_type != file2.file_type || file1.inode_num != file2.inode_num) return FAIL;
 	return PASS;
 }
 
+/* print_file_test
+ * 
+ * Inputs: fname -- name of file to print
+ * Outputs: None
+ * Side Effects: Tests file_read and read_dentry_by_name
+ * Coverage: FILE SYSTEM
+ * Files: file_system.c
+ */
 int print_file_test(const int8_t* fname){
 	TEST_HEADER;
 
@@ -260,20 +280,33 @@ int print_file_test(const int8_t* fname){
 
 	if(-1 == read_dentry_by_name(fname, &file)) return FAIL;
 
-	clear	();
+	clear();
 
+	// Prints data about the file as well as its contents to the screen
 	print_file(&file);
 
 	return PASS;
 }
 
-int test_dir_open(){
+/* test_dir_read
+ * 
+ * Inputs: None
+ * Outputs: None
+ * Side Effects: Tests dir_read
+ * Coverage: FILE SYSTEM
+ * Files: file_system.c
+ */
+int test_dir_read(){
+	TEST_HEADER;
 	uint32_t  offset = 0;
 	int32_t fd;
 	uint8_t buf[32];
 	int i;
 	int num_bytes;
+
+	// As long as dir_read is not returning 0 (it has not reached the end of the file), read in the next directory
 	while((num_bytes = dir_read (&fd , buf, 32, &offset))){
+		// Print the name of the directory to screen
 		for(i = 0; i<num_bytes; i++){
 			putc(buf[i]);
 		}
@@ -282,18 +315,39 @@ int test_dir_open(){
 	return PASS;
 }
 
-int open_file(const int8_t* fname){
+/* test_open_file
+ * 
+ * Inputs: fname -- name of file to open
+ * Outputs: None
+ * Side Effects: Tests file_open and file_close
+ * Coverage: FILE SYSTEM
+ * Files: file_system.c
+ */
+int test_open_file(const int8_t* fname){
+	TEST_HEADER;
 	int32_t fd;
+	// Open and then close the given file
 	file_open((uint8_t*)fname,&fd);
 	printf("%d",fd);
 	file_close(&fd);
+
+	// If close was successful, fd should be 0, if it isnt return FAIL
 	if(fd==0){
 		return PASS;
 	}
 	return FAIL;
 }
 
-int open_dir(const int8_t* fname){
+/* test_open_dir
+ * 
+ * Inputs: fname -- name of directory to open
+ * Outputs: None
+ * Side Effects: Tests dir_open
+ * Coverage: FILE SYSTEM
+ * Files: file_system.c
+ */
+int test_open_dir(const int8_t* fname){
+	TEST_HEADER;
 	int32_t fd;
 	if ( 0 == dir_open((uint8_t*)fname,&fd)) return PASS;
 	else return FAIL;
@@ -315,9 +369,15 @@ void launch_tests(){
 	// rtc_test();
 	// syscall_test();
 	// rtc_write_test();
-	// TEST_OUTPUT("test_dir_open", test_dir_open());
-	// TEST_OUTPUT("print_file_test", print_file_test("frame1.txt"));
-	// terminal_keyboard_test();
-	//TEST_OUTPUT("open_file",open_file("frame1.txt"));
-	TEST_OUTPUT("open_dir",open_dir("frame1.txt"));
+	
+	
+	// CP2 tests
+
+	//TEST_OUTPUT("rtc_write_test", rtc_write_test());
+	TEST_OUTPUT("read_dentry_test", read_dentry_test("frame1.txt"));
+	TEST_OUTPUT("print_file_test", print_file_test("frame1.txt"));
+	TEST_OUTPUT("test_dir_read", test_dir_read());
+	TEST_OUTPUT("test_open_file",test_open_file("frame1.txt"));
+	TEST_OUTPUT("test_open_dir",test_open_dir("frame1.txt"));
+	terminal_keyboard_test();
 }
