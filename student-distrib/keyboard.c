1061adc80627693b46cfcfdc1f79bebd4d028fa2
mrnolan3 <mrnolan3@illinois.edu>
2020-05-04 14:57:58 -0500

    Replace keyboard.c

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -1,19 +1,26 @@
 #include "keyboard.h"
+#include "paging.h"
 #include "int_wrapper.h"
+//#include "lib.c"
+#include "syscall_handler.h"
 
-static int read_lock = 0;
-static int kbrd_buf_length = 0;
-static int is_enter = 0;
-static int is_shift = 0;
-static int is_caps = 0;
-static int is_ctrl = 0;
+static int read_lock[MAX_TERMINALS] = {0, 0, 0};
+static int kbrd_buf_length[MAX_TERMINALS] = {0, 0, 0};
+static int is_enter[MAX_TERMINALS] = {0, 0, 0};
+static int is_shift[MAX_TERMINALS] = {0, 0, 0};
+static int is_caps[MAX_TERMINALS] = {0, 0, 0};
+static int is_ctrl[MAX_TERMINALS] = {0, 0, 0};
+static int is_alt[MAX_TERMINALS] = {0, 0, 0};
+static int current_terminal;
 
 
 // keyboard buffer
-static int8_t kbrd_buf[KBRD_BUF_LENGTH];
+static int8_t kbrd_buf[MAX_TERMINALS][KBRD_BUF_LENGTH];
 static int8_t kbrd_read_buf[KBRD_BUF_LENGTH];
+
+  
 // int to keep track of location in keyboard buf
-static uint8_t line_marker = 0;
+static uint8_t line_marker[MAX_TERMINALS] = {0, 0, 0};
 
 /*
 * terminal_open
@@ -62,13 +69,13 @@ int32_t terminal_read(int32_t fd, uint8_t* buf, int32_t nbytes)
     }
 
     // initialize reader and set enter check to false
-    read_lock = 1;
-    is_enter = 0;
+    read_lock[current_terminal] = 1;
+    is_enter[current_terminal] = 0;
 
     
 
     // wait for enter input
-    while(!is_enter);
+    while(!is_enter[get_curr_pcb_term()]);
 
     // save flags
     cli();
@@ -77,14 +84,14 @@ int32_t terminal_read(int32_t fd, uint8_t* buf, int32_t nbytes)
     int i;
 
     // Make sure nbytes is positive, if not make it 0
-	  if(line_marker - nbytes < 0)
+	  if(line_marker[current_terminal] - nbytes < 0)
     {
         writeBytes = 0;
     } 
     else
     {
         // set writebytes to the bytes to be written
-        writeBytes = line_marker - (nbytes - 1);
+        writeBytes = line_marker[current_terminal] - (nbytes - 1);
     }
     
     /* 
@@ -98,36 +105,36 @@ int32_t terminal_read(int32_t fd, uint8_t* buf, int32_t nbytes)
         // write the first nbytes values in the kbrd buffer into the transfer buffer
         for(i = 0; i < nbytes - 1; i++)
         {
-            kbrd_read_buf[i] = kbrd_buf[i];
+            kbrd_read_buf[i] = kbrd_buf[current_terminal][i];
         }
         //Set the input buf to be the first nbytes in the keyboard buffer
         //and append /n to the last value (overwriting the value if its exactly the size of the buffer)
         kbrd_read_buf[nbytes - 1] = '\n';
         memcpy(buf, kbrd_read_buf, nbytes);
-        memmove(kbrd_buf, kbrd_buf + nbytes - 1, writeBytes);
-        memset(kbrd_buf + writeBytes, NULL_SCANCODE, nbytes);
-        line_marker = writeBytes;
+        memmove(kbrd_buf[current_terminal], kbrd_buf[current_terminal] + nbytes - 1, writeBytes);
+        memset(kbrd_buf[current_terminal] + writeBytes, NULL_SCANCODE, nbytes);
+        line_marker[current_terminal] = writeBytes;
     }
     else
     {
         // write the first nbytes values in the kbrd buffer into the transfer buffer
         for(i = 0; i < KBRD_BUF_LENGTH - 1; i++)
         {
-            kbrd_read_buf[i] = kbrd_buf[i];
+            kbrd_read_buf[i] = kbrd_buf[current_terminal][i];
         }
         //Set the input buf to be the first nbytes in the keyboard buffer
         //and append /n to the last value (overwriting the value if its exactly the size of the buffer)
         kbrd_read_buf[KBRD_BUF_LENGTH - 1] = '\n';
         memcpy(buf, kbrd_read_buf, KBRD_BUF_LENGTH);
-        memmove(kbrd_buf, kbrd_buf + KBRD_BUF_LENGTH - 1, writeBytes);
-        memset(kbrd_buf + writeBytes, NULL_SCANCODE, KBRD_BUF_LENGTH);
-        line_marker = writeBytes;
+        memmove(kbrd_buf[current_terminal], kbrd_buf[current_terminal] + KBRD_BUF_LENGTH - 1, writeBytes);
+        memset(kbrd_buf[current_terminal] + writeBytes, NULL_SCANCODE, KBRD_BUF_LENGTH);
+        line_marker[current_terminal] = writeBytes;
     }
     
     // get flags back, relinquish read lock and set is_enter back to false, then return
     sti();
-    read_lock = 0;
-    is_enter = 0;
+    read_lock[current_terminal] = 0;
+    is_enter[current_terminal] = 0;
     int retval = (nbytes < KBRD_BUF_LENGTH) ? nbytes : KBRD_BUF_LENGTH;
     return retval;
 }
@@ -158,7 +165,7 @@ int32_t terminal_write(int32_t fd, const uint8_t* buf, int32_t nbytes)
     // print first nbytes characters of buffer to screen
     for(i = 0; i < nbytes; i++)
     {
-      putc(buf[i]);
+      putc(buf[i], get_curr_pcb_term());
     }
   
     // retrieve flags and return
@@ -180,12 +187,16 @@ int32_t terminal_write(int32_t fd, const uint8_t* buf, int32_t nbytes)
 */
 void kbrd_init() {
     // set shift, caps and ctrl check variables to false
-    is_shift = 0;
-    is_caps = 0;
-    is_ctrl = 0;
+    is_shift[current_terminal] = 0;
+    is_caps[current_terminal] = 0;
+    is_ctrl[current_terminal] = 0;
+    is_alt[current_terminal] = 0;
 
     // set keyboard buffer to be completely null characters
-    memset(kbrd_buf, NULL_SCANCODE, KBRD_BUF_LENGTH);
+    memset(kbrd_buf[current_terminal], NULL_SCANCODE, KBRD_BUF_LENGTH);
+
+    current_terminal = 0;
+    update_curr_term(0);
 
     // enable keyboard irq port
     enable_irq(KBRD_IRQ);
@@ -218,90 +229,129 @@ void kbrd_int() {
       // set ctrl, shift and caps variables based on pressed keys
       if(scancode == LCTRL_PRESS)
       {
-        is_ctrl = 1;
+        is_ctrl[current_terminal] = 1;
       }
       else if(scancode == LCTRL_RELEASE)
       {
-        is_ctrl = 0;
+        is_ctrl[current_terminal] = 0;
       }
       else if(scancode == LSHIFT_PRESS || scancode == RSHIFT_PRESS)
       {
-        is_shift = 1;
+        is_shift[current_terminal] = 1;
       }
       else if(scancode == LSHIFT_RELEASE || scancode == RSHIFT_RELEASE)
       {
-        is_shift = 0;
+        is_shift[current_terminal] = 0;
+      }
+      else if(scancode == LALT_PRESS)
+      {
+        is_alt[current_terminal] = 1;
+      }
+      else if(scancode == LALT_RELEASE)
+      {
+        is_alt[current_terminal] = 0;
       }
       else if(scancode == CAPS_PRESS)
       {
-          if(is_caps == 1)
+          if(is_caps[current_terminal] == 1)
           {
-            is_caps = 0;
+            is_caps[current_terminal] = 0;
           }
           else
           {
-            is_caps = 1;
+            is_caps[current_terminal] = 1;
           }
+      }
+      else if(scancode == F1_SCANCODE && is_alt[current_terminal] == 1)
+      {
+        if(current_terminal != 0)
+        {
+            current_terminal = 0;
+            update_curr_term(0);
+            update_vidmem(0);
+            // change_vidmap(0);
+            
+        }
+      }
+      else if(scancode == F2_SCANCODE && is_alt[current_terminal] == 1)
+      {
+        if(current_terminal != 1)
+        {
+            current_terminal = 1;
+            update_curr_term(1);
+            update_vidmem(1);
+            // change_vidmap(1);
+        }
+      }
+      else if(scancode == F3_SCANCODE && is_alt[current_terminal] == 1)
+      {
+        if(current_terminal != SECOND_TERMINAL_CASE)
+        {
+            current_terminal = SECOND_TERMINAL_CASE;
+            update_curr_term(SECOND_TERMINAL_CASE);
+            update_vidmem(SECOND_TERMINAL_CASE);
+            // change_vidmap(2);
+        }
       }
       // If enter is pressed, go to a new line, set is_enter to true and reset kbrd buffer if not being read from
       else if(scancode == ENTER_PRESS)
       {
           // Out put newline char to move to newline
-          putc(key);
+          putc(key, current_terminal);
 
           // Set is_enter variable to true
-          is_enter = 1;
+          is_enter[current_terminal] = 1;
 
           // Reset kbrd buffer if not reading
-          kbrd_buf_length = 0;
-          if(read_lock == 0)
+          kbrd_buf_length[current_terminal] = 0;
+          if(read_lock[current_terminal] == 0)
           {
-              memset(kbrd_buf, NULL_SCANCODE, KBRD_BUF_LENGTH);
-			        line_marker = 0;
+              memset(kbrd_buf[current_terminal], NULL_SCANCODE, KBRD_BUF_LENGTH);
+			        line_marker[current_terminal] = 0;
           }
       }
       // If backspace is pressed, call backspace helper function and remove char from keyboard buffer
       else if(scancode == BACKSPACE_PRESS)
       {
           // Make sure keyboard buffer holds values
-          if(kbrd_buf_length > 0)
+          if(kbrd_buf_length[current_terminal] > 0)
           {
-              backspace_helper();
-              kbrd_buf[line_marker - 1] = NULL_SCANCODE;
-              kbrd_buf_length--;
-              line_marker--;
+              backspace_helper(current_terminal);
+              kbrd_buf[current_terminal][line_marker[current_terminal] - 1] = NULL_SCANCODE;
+              kbrd_buf_length[current_terminal]--;
+              line_marker[current_terminal]--;
           }
       }
       // If keyboard buffer is less than the max allowed size and the scancode is valid
       // print it to the screen and add the character to the buffer
-      else if((kbrd_buf_length < KBRD_BUF_LENGTH - 1) && scancode < SCANCODE_LENGTH)
+      else if((kbrd_buf_length[current_terminal] < KBRD_BUF_LENGTH - 1) && scancode < SCANCODE_LENGTH)
       {
         // If ctrl key is pressed along with L, clear screen, reset cursor and puts the current state of buffer
-        if(is_ctrl == 1 && key == L_SCANCODE)
+        if(is_ctrl[current_terminal] == 1 && key == L_SCANCODE)
         {
           reset();
-          puts(kbrd_buf);
+          puts(kbrd_buf[current_terminal], current_terminal);
         }
         else
         {
             // Set the key based on whether shift, caps lock or both are active
-            if(is_shift == 1 && is_caps == 0)
+            if(is_shift[current_terminal] == 1 && is_caps[current_terminal] == 0)
             {
                 key = shiftkbrdscancode[scancode];
             }
-            else if(is_shift == 1 && is_caps == 1)
+            else if(is_shift[current_terminal] == 1 && is_caps[current_terminal] == 1)
             {
                 key = capsshiftkbrdscancode[scancode];
             }
-            else if(is_shift == 0 && is_caps == 1)
+            else if(is_shift[current_terminal] == 0 && is_caps[current_terminal] == 1)
             {
                 key = capskbrdscancode[scancode];
             }
             // Output the key to the screen and the character to the buffer
-            putc(key);
-            kbrd_buf[line_marker] = key;
-            line_marker++;
-            kbrd_buf_length++;
+            putc(key, current_terminal);
+            kbrd_buf[current_terminal][line_marker[current_terminal]] = key;
+            line_marker[current_terminal]++;
+            kbrd_buf_length[current_terminal]++;
         }
       }
     }
